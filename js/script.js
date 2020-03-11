cb9f72c7dc41868c746ce3846fd577255f7ac01a
carlosalabau <carlytos347@gmail.com>
2020-03-11 18:10:07 +0100

    Funciones añadidas

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@
 //         main.innerHTML += `<div class="pelicula"><h1>Titulo: ${pelicula.title}</h1></div>`;
 //     });
 //     });
+
 function buscarPeliculas() {
   let main = document.querySelector('#columna');
   let buscar = document.querySelector('#buscador').value;
   let url = 'https://api.themoviedb.org/3/search/movie?api_key=f4ae829d35c414bf37cb8b3989937a71&query=';
-  let imagen = document.querySelector('i').src;
 
   axios.get(url + buscar)
     .then(function(res) {
@@ -27,14 +27,35 @@ function buscarPeliculas() {
            <img src="https://image.tmdb.org/t/p/original${pelicula.poster_path}" class="card-img-top i${pelicula.id}" alt="">
            <div class="card-body">
            <h5 class="card-title">${pelicula.title}</h5>
-           <p class="card-text n${pelicula.id}">${pelicula.overview}</p>
-           <p><button type="button" onclick="verMas(${pelicula.id})">Ver mas</button></p>
+           <p class="card-text b${pelicula.id}">${pelicula.overview}</p>
+
            <button class="btn btn-primary">Ver pelicula</button>
            </div>
           </div>`;
+        document.querySelector('.card-img-top').addEventListener("load", function() {
+          cambiarImg(pelicula.id)
+        });
+        document.querySelector('.card-text').addEventListener("load", function(){
+          addBoton(pelicula.overview.length,pelicula.id);
+        })
       };
     });
 }
-function verMas(id){
-  document.querySelector('.n'+id).style.cssText='overflow:visible; height:auto';
+
+function cambiarImg(id) {
+  let imagen = document.querySelector('.i' + id);
+  if (imagen.src == 'https://image.tmdb.org/t/p/originalnull') {
+    imagen.src = 'img/nofoto.jpg';
+  }
+}
+function addBoton(palabras,id){
+  console.log('entro!')
+  let texto = document.querySelector('.b'+id);
+  let boton = '<button class="n"'+id+" >Ver mas</button>"
+  if(palabras > 190){
+    texto.appendChild(boton);
+  }
+}
+function verMas(id) {
+  document.querySelector('.n' + id).style.cssText = 'overflow:visible; height:auto';
 }
