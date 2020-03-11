2e2e0ddbb5d091bd3582397d3820d42ce0ab9762
carlosalabau <carlytos347@gmail.com>
2020-03-11 16:46:05 +0100

    Problemas corregidos

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
 /* const axios = require('axios'); */
+// console.log(buscar);
+//
+//
+// fetch('https://api.themoviedb.org/3/search/movie?api_key=f4ae829d35c414bf37cb8b3989937a71&page=1&query='+buscar)
+// .then(res => res.json())
+// .then(res => {
+//     let peliculas = res.results;
+//     console.log(peliculas);
+//     peliculas.forEach(pelicula => {
+//         console.log(pelicula.title);
+//         main.innerHTML += `<div class="pelicula"><h1>Titulo: ${pelicula.title}</h1></div>`;
+//     });
+//     });
 
-let buscar = document.querySelector('#buscador').value;
-let main = document.querySelector('main');
-function buscarPeliculas(){
-/* axios.get('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=en-US&page=1&include_adult=false&query='+buscar)
-.then(function(res){
-        let peliculas = res.data.results;
-        console.log(peliculas)
-}) */
-console.log(buscar);
 
+function buscarPeliculas() {
+  let main = document.querySelector('#columna');
+  let buscar = document.querySelector('#buscador').value;
+  let url = 'https://api.themoviedb.org/3/search/movie?api_key=f4ae829d35c414bf37cb8b3989937a71&query=';
+  let imagen = document.querySelector('i').src;
 
-fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=en-US&page=1&include_adult=false&query='+buscar)
-.then(res => res.json())
-.then(res => {
-    const peliculas = res.results;
-    peliculas.forEach(pelicula => {
-        console.log(pelicula.title);
-        main.innerHTML += `<div class="pelicula"><h1>Titulo: ${pelicula.title}</h1></div>`;
+  axios.get(url + buscar)
+    .then(function(res) {
+      let peliculas = res.data.results;
+      for (let pelicula of peliculas) {
+        main.innerHTML += `
+           <div class="card col-lg-3 col-sm-4 col-xs-6 col-12">
+           <img src="https://image.tmdb.org/t/p/original${pelicula.poster_path}" class="card-img-top i${pelicula.id}" alt="">
+           <div class="card-body">
+           <h5 class="card-title">${pelicula.title}</h5>
+           <p class="card-text n${pelicula.id}">${pelicula.overview}</p>
+           <p><button type="button" onclick="verMas(${pelicula.id})">Ver mas</button></p>
+           <button class="btn btn-primary">Ver pelicula</button>
+           </div>
+          </div>`;
+      };
     });
-    });
+}
+function verMas(id){
+  document.querySelector('.n'+id).style.cssText='overflow:visible; height:auto';
 }
